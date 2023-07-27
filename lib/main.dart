3c789681b31f3537ad10dc1d0e0a1347d0287c70
Tomasz <tomasz.golec.97@gmail.com>
2023-07-27 13:48:36 +0200

    Added description, first styling to expenses items

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: MyHomePage(),
@@ -20,14 +20,68 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: '1', title: 'Food', amount: 12.99, date: DateTime.now()),
+    Transaction(id: '2', title: 'Games', amount: 200, date: DateTime.now()),
+    Transaction(id: '3', title: 'Clothes', amount: 69.99, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: const Text('Flutter App'),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Text('Widget'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const SizedBox(
+            width: double.infinity,
+            child: Card(
+              elevation: 2,
+              color: Colors.blue,
+              child: Text('Chart!'),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      border: Border.all(color: Colors.green, width: 2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: const TextStyle(
+                          color: Colors.green, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(tx.date.toString(),
+                          style: const TextStyle(color: Colors.grey)),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
